ac2569a6f79b672a5ea552e4b3bb817375f11389
Lenny <lenny.rincon.5@gmail.com>
2026-04-09 17:45:04 -0500

    Corrección de conexión del registro

--- diseno_ux/lib/core/constants.dart
+++ diseno_ux/lib/core/constants.dart
@@ -1,3 +1,11 @@
-const BASE_URL = 'http://10.0.2.2:3000/api/v1';
+import 'package:flutter/foundation.dart';
+
+const String _androidEmulatorBaseUrl = 'http://10.0.2.2:3000/api/v1';
+const String _localBaseUrl = 'http://127.0.0.1:3000/api/v1';
+
+final String BASE_URL =
+	defaultTargetPlatform == TargetPlatform.android
+	? _androidEmulatorBaseUrl
+	: _localBaseUrl;
 const TIMEOUT_SECONDS = 30;
 const TOKEN_KEY = 'token';
