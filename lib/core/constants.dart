22d861f917276926190724c7b24632408b358bb5
Pedro Mateo Espinel Domínguez <mateodomingueze@gmail.com>
2026-04-13 18:33:44 -0500

    Api
    El aplicativo movil ahora puede interactuar con la api-docker

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/foundation.dart';
 
-const String _androidEmulatorBaseUrl = 'http://10.0.2.2:3000/api/v1';
-const String _localBaseUrl = 'http://127.0.0.1:3000/api/v1';
+// Reemplaza 192.168.1.XX con tu IP real
+const String _androidEmulatorBaseUrl = 'http://10.0.2.2:8000/api/v1';
+const String _physicalDeviceBaseUrl = 'http://192.168.1.2:8000/api/v1';
+const String _localBaseUrl = 'http://127.0.0.1:8000/api/v1';
 
-final String BASE_URL =
-	defaultTargetPlatform == TargetPlatform.android
-	? _androidEmulatorBaseUrl
-	: _localBaseUrl;
+final String BASE_URL = defaultTargetPlatform == TargetPlatform.android
+    ? _physicalDeviceBaseUrl // ← Cambia esto cuando uses emulador
+    : _localBaseUrl;
 const TIMEOUT_SECONDS = 30;
 const TOKEN_KEY = 'token';
